6499048369c08744af2645345169d85b05e79237
serina <167188746+sirinemerai19@users.noreply.github.com>
2025-03-01 17:08:24 +0100

    updates

--- connection.cpp
+++ connection.cpp
@@ -8,9 +8,9 @@ Connection::Connection()
 bool Connection::createconnect()
 {bool test=false;
     QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-    db.setDatabaseName("Source_Projet2A");//inserer le nom de la source de données
-    db.setUserName("shopdevs");//inserer nom de l'utilisateur
-    db.setPassword("mallflow");//inserer mot de passe de cet utilisateur
+    db.setDatabaseName("ProjetCPP");
+    db.setUserName("Slim");
+    db.setPassword("Slim");
 
     if (db.open())
         test=true;
